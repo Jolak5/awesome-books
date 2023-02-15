1b434ceacd19feed0cead59b33006d7a823198ef
jolak5 <kayodej4@gmail.com>
2023-02-15 09:56:03 +0100

    implement navigation bar

--- classes.js
+++ classes.js
@@ -9,13 +9,13 @@ const switchMode = (node) => {
   if (showListButton !== node && showListButton.classList.contains('active')) {
     showListButton.classList.remove('active');
   } else if (
-    addNewButton !== node
-    && addNewButton.classList.contains('active')
+    addNewButton !== node &&
+    addNewButton.classList.contains('active')
   ) {
     addNewButton.classList.remove('active');
   } else if (
-    contactInfoButton !== node
-    && contactInfoButton.classList.contains('active')
+    contactInfoButton !== node &&
+    contactInfoButton.classList.contains('active')
   ) {
     contactInfoButton.classList.remove('active');
   }
